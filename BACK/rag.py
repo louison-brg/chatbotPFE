75381569aaa12c9ab27b6376721275ff2fe7022c
louison beranger <beranger.louison@yahoo.fr>
2024-12-06 16:01:52 +0100

    Fichier json test + fichier rag python non fonctionnel

---
+++ BACK/rag.py
@@ -0,0 +1,22 @@
+from langchain_ollama import OllamaEmbeddings
+
+embeddings = OllamaEmbeddings(
+    model="phi3",
+)
+
+
+text = "LangChain is the framework for building context-aware reasoning applications"
+
+vectorstore = InMemoryVectorStore.from_texts(
+    [text],
+    embedding=embeddings,
+)
+
+# Use the vectorstore as a retriever
+retriever = vectorstore.as_retriever()
+
+# Retrieve the most similar text
+retrieved_documents = retriever.invoke("What is LangChain?")
+
+# show the retrieved document's content
+retrieved_documents[0].page_content
